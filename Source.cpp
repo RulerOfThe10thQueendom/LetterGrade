9ebb0ca712bfa286ae5c8c5e77ced4a9e85a44a3
Ruler of the 10th Queendom <yourfriend@10thQueendom.com>
2020-03-03 22:54:00 -0500

    Update Source.cpp
    Better Edition

--- Source.cpp
+++ Source.cpp
@@ -9,28 +9,34 @@ int main()
 	cout << "What grade did you make? ";
 	cin >> grade;
 
-	if (grade = 100)
+	if (grade > 100)
+		cout << "That grade isn't possible.\n";
+	else if(grade == 100)
 		cout << "Congratulations, you got a perfect score: A+\n";
-	if (grade >= 90)
-	{
-		if (grade < 100)
+	else if(grade >= 93)
 			cout << "Very good: A\n";
-	}
-	if (grade >= 80)
-		if (grade < 90)
+	else if (grade >= 90)
+		cout << "Very good: A-\n";
+	else if (grade >= 87)
+		cout << "Good job: B+\n";
+	else if(grade >= 83)
 			cout << "Good job: B\n";
-	if (grade <= 70)
-	{
-		if (grade < 80)
-			cout << "Not bad: C\n";
-	}
-	if (grade > 60)
-	{
-		if (grade < 70)
-			cout << "Not so good: D\n";
-	}
-	if (grade < 60)
-		cout << "We need to talk: F\n";
+	else if (grade >= 80)
+		cout << "Good job: B-\n";
+	else if (grade >= 77)
+		cout << "Not bad: C+\n";
+	else if (grade >= 73)
+		cout << "Not bad: C\n";
+	else if (grade >= 70)
+			cout << "Not bad: C-\n";
+	else if(grade >= 67)
+			cout << "Not so good: D+\n";
+	else if (grade >= 60)
+		cout << "Not so good: D\n";
+	else if (grade >= 0)
+			cout << "We need to talk: F\n";
+	else
+		cout << "That grade isn't possible.\n";
 
 	return 0;
 }
